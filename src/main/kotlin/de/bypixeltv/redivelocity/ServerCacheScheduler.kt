605476f1e0a60c0fc3a933e3cde39dc950ed9347
Lukas <113579672+byPixelTV@users.noreply.github.com>
2024-07-05 12:08:31 +0200

    Removed some debug, I forgot to remove

--- src/main/kotlin/de/bypixeltv/redivelocity/ServerCacheScheduler.kt
+++ src/main/kotlin/de/bypixeltv/redivelocity/ServerCacheScheduler.kt
@@ -6,16 +6,12 @@ import java.util.concurrent.TimeUnit
 class ServerCacheScheduler(private val plugin: RediVelocity, private val redisController: RedisController, private val proxyId: String) {
     init {
         val task = Runnable {
-            plugin.sendLogs("Updating server cache...")
             val allServers = plugin.proxy.allServers
-            println("All servers: $allServers")
             allServers.forEach { server ->
-                println("Server: ${server.serverInfo.name} - Players: ${server.playersConnected}")
                 redisController.setHashField("rv-$proxyId-servers-servers", server.serverInfo.name, server.serverInfo.address.toString())
                 redisController.setHashField("rv-$proxyId-servers-playercount", server.serverInfo.name, server.playersConnected.size.toString())
                 redisController.setHashField("rv-$proxyId-servers-address", server.serverInfo.name, server.serverInfo.address.toString())
             }
-            plugin.sendLogs("Updated server cache")
         }
 
         // Schedule the task to run every 30 seconds
