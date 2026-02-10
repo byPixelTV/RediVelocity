2c753f75e32399319e7240f5e1b9a4d626945b81
TheCGuyGitHub <thecguy@vulpescloud.de>
2026-02-10 19:26:05 +0100

    fix: Correct VulpesCloud version

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,7 @@ dependencies {
 
     compileOnly("app.simplecloud.api.platform:velocity:0.0.5-dev.1745077021664-28517d8")
 
-    val vulpesCloudVersion = "3.0.0"
+    val vulpesCloudVersion = "3.0.0-beta1"
     compileOnly("de.vulpescloud", "bridge", vulpesCloudVersion)
     compileOnly("de.vulpescloud", "api", vulpesCloudVersion)
 
